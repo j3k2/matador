0961859e67280e4c1c38bfd85e21bbde8c747e48
John Kim <jksfo@pm.me>
2020-05-07 14:22:37 -0700

    initial commit

---
+++ src/components/MatadorChart.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import request from 'superagent';
+import { ResponsiveLine } from '@nivo/line'
+import { format, sub } from 'date-fns'
+
+export default class MatadorChart extends React.Component {
+  state = {
+    data: [],
+    symbol: ''
+  }
+
+  setSymbol = (event) => {
+    this.setState({
+      symbol: event.target.value
+    })
+  }
+
+  getData = (event) => {
+    event.preventDefault();
+    const end = format(new Date(), 'yyyy-MM-dd');
+    const start = format(sub(new Date(), { months: 1 }), 'yyyy-MM-dd');
+    request.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${this.state.symbol}?from=${start}&to=${end}`)
+      .then((res) => {
+        if (res.body.historical) {
+          const data = res.body.historical.map((obj) => {
+            return {
+              x: obj.date,
+              y: obj.close
+            }
+          }).sort((a, b) => Date.parse(a.x) - Date.parse(b.x))
+
+          this.setState({
+            data: [...this.state.data, {
+              id: res.body.symbol,
+              data: data
+            }]
+          })
+        } else {
+          //not found
+        }
+      })
+  }
+
+  render() {
+    return <div style={{ height: 600 }}>
+      <form onSubmit={this.getData}>
+        <input type='text' value={this.state.symbol} onChange={this.setSymbol}></input>
+      </form>
+      <ResponsiveLine
+        data={this.state.data}
+        margin={{ top: 50, right: 100, bottom: 50, left: 50 }}
+        xScale={{
+          type: 'time',
+          format: '%Y-%m-%d'
+        }}
+        xFormat="time:%Y-%m-%d"
+        enableSlices="x"
+        sliceTooltip={({ slice }) => {
+          return (
+            <div
+              style={{
+                background: 'white',
+                padding: '9px 12px',
+                border: '1px solid black',
+              }}
+            >
+              <div>
+                {slice.points[0].data.xFormatted}
+              </div>
+              {slice.points.sort((a, b) => b.data.yFormatted - a.data.yFormatted).map(point => (
+                <div
+                  key={point.id}
+                  style={{
+                    color: point.serieColor,
+                    padding: '3px 0px',
+                  }}
+                >
+                  <strong>{point.serieId}</strong> ${point.data.yFormatted}
+                </div>
+              ))}
+            </div>
+          )
+        }}
+        axisBottom={{
+          format: '%b %d',
+          tickValues: 'every 2 days',
+        }}
+        axisLeft={{
+          format: value =>
+            `$${value}`
+        }}
+        colors={{ scheme: 'category10' }}
+        legends={[
+          {
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: 'left-to-right',
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 6,
+            symbolShape: 'circle',
+            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1
+                }
+              }
+            ],
+            onClick: (d) => {
+              console.log(d);
+            }
+          }
+        ]}
+      >
+      </ResponsiveLine></div>
+  }
+}
